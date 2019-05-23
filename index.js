ea108fe2901da81f2e496d60db4e729835a35d4c
mome <diablo3foracount@gmail.com>
2019-05-23 22:13:51 +0900

    a

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Discord Bot Activated.This page can close.");
+
+});
 var port = process.env.PORT || 3000;
 server.listen(port);
 // discord.js モジュールのインポート
